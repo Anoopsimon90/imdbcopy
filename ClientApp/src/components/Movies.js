adfeaf23a7a43c5f96811db51c0b1dbdf3ca9da7
Anoop Simon <anoop.simon@adudev.com>
2019-04-17 16:41:53 +1000

    integrated API for movie search

--- ClientApp/src/components/Movies.js
+++ ClientApp/src/components/Movies.js
@@ -1,14 +1,37 @@
 ﻿import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { FormControl, Button, Form, Table } from 'react-bootstrap';
+
 export class Movies extends Component {
     constructor(props) {
         super(props);
     };
+
+    static searchMovie() {
+        fetch('http://localhost:61443/api/movies/Ter')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                return data;
+
+            })
+    }
     render() {
 
         return (
+
             <div>
-                <p>Search Movies</p>
+                <Table data={Movies.searchMovie()}>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <tr> <td> </td></tr>
+                    </tr>
+                </Table>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className=" mr-sm-2" />
+                    <Button type="submit">Submit</Button>
+                </Form>
             </div>
 
         );
